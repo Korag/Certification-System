b62a910e59d9cee72c933283c0286c7ab0d09c13
Łukasz Czepielik <koragrex@gmail.com>
2019-03-30 15:37:12 +0100

    Added Data Access Layer - MongoContext, DatabaseOperations interface and specific Mongo Operation class.

---
+++ Certification_System/DAL/MongoContext.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Certification_System.DAL
+{
+    public class MongoContext
+    {
+        private string _user;
+        private string _password;
+        private string _database;
+        private string _connectionstring;
+
+        private MongoClient _mongoClient;
+
+        public IMongoDatabase db;
+
+        public MongoContext()
+        {
+            InitializeDatabaseCredentials();
+            InitializeContextInstantion();
+        }
+
+        private void InitializeDatabaseCredentials()
+        {
+            _user = "Admin";
+            _password = "certification_admin1";
+            _database = "certification_system";
+            //_connectionstring = $"mongodb://{_user}:{_password}@ds026898.mlab.com:26898/{_database}";
+
+            _connectionstring = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        private void InitializeContextInstantion()
+        {
+            _mongoClient = new MongoClient(_connectionstring);
+            db = _mongoClient.GetDatabase(_database);
+        }
+
+
+
+
+    }
+}
---
+++ Certification_System/DAL/MongoOperations.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.AspNet.Identity;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Certification_System.DAL
+{
+    public class MongoOperations : IDatabaseOperations
+    {
+        private MongoContext _context;
+
+        private string _usersCollectionName = "AspNetUsers";
+
+        //Collections
+        private IMongoCollection<IdentityUser> _users;
+
+        public MongoOperations()
+        {
+            _context = new MongoContext();
+        }
+
+        #region Methods
+        private List<IdentityUser> GetUsers()
+        {
+            _users = _context.db.GetCollection<IdentityUser>(_usersCollectionName);
+            return _users.AsQueryable().ToList();
+        }
+        #endregion
+    }
+}
