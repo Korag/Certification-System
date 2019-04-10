c49c4e4f5a6bc0a865b053ef572cbc9d28446766
Łukasz Czepielik <koragrex@gmail.com>
2019-04-11 00:15:59 +0200

    Added autofac container to the project.

---
+++ Certification_System/App_Start/AutofacConfig.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Certification_System.DAL;
+
+namespace Certification_System.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MongoOperations>().As<IDatabaseOperations>().InstancePerRequest();
+            builder.RegisterType<MongoContext>().AsSelf().InstancePerRequest();
+            var container = builder.Build();
+        }
+    }
+}
