44bceb355603a4da765b8391e2ea776d3bfb3b55
Łukasz Czepielik <koragrex@gmail.com>
2019-04-14 00:02:51 +0200

    Added Roles to Layout. Now Worker, Company and Admin have access only to specific methods. It is first try to make things right.

--- Certification_System/Controllers/HomeController.cs
+++ Certification_System/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,10 +14,11 @@ namespace Certification_System.Controllers
             return View();
         }
 
+        [Authorize(Roles = "Admin")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+         
             return View();
         }
 
