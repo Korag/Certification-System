f6e1b668fe28a44e0c827ef03d7e57fa79977d31
Łukasz Czepielik <koragrex@gmail.com>
2019-04-14 01:07:30 +0200

    Changed ChangePassword View with adjusting frontend to another management formulao. Moreover changed ViewModel - ChangePasswordViewModel from ManageViewModels with translated names of fields and also small changes to validation.

--- Certification_System/Models/ManageViewModels.cs
+++ Certification_System/Models/ManageViewModels.cs
@@ -43,18 +43,18 @@ namespace Certification_System.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Dotychczasowe hasło")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Nowe hasło musi posiadać conajmniej {2} znaków.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nowe hasło")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Potwierdź nowe hasło")]
+        [Compare("NewPassword", ErrorMessage = "Wprowadzone hasła nie są identyczne.")]
         public string ConfirmPassword { get; set; }
     }
 
