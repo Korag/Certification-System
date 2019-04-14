35a401e4f363ee40ad0fece6603b1c92ffac6d0e
Łukasz Czepielik <koragrex@gmail.com>
2019-04-15 00:02:09 +0200

    Added Instructor MongoDb Collection Model class.

--- Certification_System/Models/Instructor.cs
+++ Certification_System/Models/Instructor.cs
@@ -7,18 +7,18 @@ using System.Web;
 
 namespace Certification_System.Models
 {
-    public class Meeting
+    public class Instructor
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public DateTime DateOfMeeting { get; set; }
-        public List<string> AttendanceList { get; set; }
-        public string InstructorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
         public string Country { get; set; }
-        public string City { get; set; }
         public string Address { get; set; }
-        public string PostCode { get; set; }
         public string NumberOfApartment { get; set; }
+
     }
 }
