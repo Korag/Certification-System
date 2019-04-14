5be63cc07e8d4a8fbf5b2bff2e7e3bb4014c93a0
Łukasz Czepielik <koragrex@gmail.com>
2019-04-14 19:20:30 +0200

    Added Company MongoDb collection Model.

--- Certification_System/Models/Company.cs
+++ Certification_System/Models/Company.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,16 @@ namespace Certification_System.Models
 {
     public class Company
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Address { get; set; }
+        public string NumberOfApartment { get; set; }
     }
 }
