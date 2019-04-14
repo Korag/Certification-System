e8324a877ba29644c8d2ba04da224f8ce80f0eeb
Łukasz Czepielik <koragrex@gmail.com>
2019-04-14 19:08:44 +0200

    Added Course MongoDb Collection model.

---
+++ Certification_System/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Certification_System.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<string> Meetings { get; set; }
+    }
+}
