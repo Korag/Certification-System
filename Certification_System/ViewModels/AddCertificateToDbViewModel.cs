e737a2fed6753958506daeba3625304a61217e00
Łukasz Czepielik <koragrex@gmail.com>
2019-04-23 01:32:59 +0200

    Refactorization of AddCertificateToDbViewModel class with added DataAnnotations.

--- Certification_System/ViewModels/AddCertificateToDbViewModel.cs
+++ Certification_System/ViewModels/AddCertificateToDbViewModel.cs
@@ -1,24 +1,25 @@
-﻿using Certification_System.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
 namespace Certification_System.ViewModels
 {
     public class AddCertificateToDbViewModel
     {
+        [Required]
+        [Display(Name="Identyfikator")]
         public string CertificateIdentificator { get; set; }
+        [Required]
+        [Display(Name = "Nazwa certyfikatu")]
         public string Name { get; set; }
+
+        [Display(Name = "Opis")]
+        [MaxLength(255)]
         public string Description { get; set; }
 
-        public ICollection<Branch> Branches { get; set; }
-        public IEnumerable<SelectListItem> BranchesChosen { get; set; }
-
-        //public IEnumerable<SelectListItem> FlavorItems
-        //{
-        //    get { return new SelectList(Branches, "Name"); 
-        //}
+        [Required(ErrorMessage = "Należy zaznaczyć conajmniej jeden Obszar.")]
+        public ICollection<string> SelectedBranches{ get; set; }
+        [Display(Name = "Obszar")]
+        public IList<SelectListItem> AvailableBranches { get; set; }
     }
 }
