d3ac64ae578f39efac4c16f58da14f38955c6aba
Łukasz Czepielik <koragrex@gmail.com>
2019-04-22 17:41:23 +0200

    Added Branch Model and refactoring of Instructor Model.

---
+++ Certification_System/Models/Branch.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Certification_System.Models
+{
+    public class Branch
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
--- Certification_System/Models/Instructor.cs
+++ Certification_System/Models/Instructor.cs
@@ -12,11 +12,13 @@ namespace Certification_System.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Country { get; set; }
+        public string City { get; set; }
         public string Address { get; set; }
         public string NumberOfApartment { get; set; }
 
