c049a94c3a6dc8296f8a22945d9809dd2c873a63
Łukasz Czepielik <koragrex@gmail.com>
2019-04-24 01:32:59 +0200

    Added AddMeetingViewModel.

---
+++ Certification_System/ViewModels/AddMeetingViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.ViewModels
+{
+    public class AddMeetingViewModel
+    {
+        [Required]
+        [Display(Name = "Identyfikator")]
+        public string MeetingIdentificator { get; set; }
+
+        [Required]
+        [Display(Name = "Data szkolenia")]
+        public DateTime DateOfMeeting { get; set; }
+
+        [Required]
+        [Display(Name = "Państwo")]
+        public string Country { get; set; }
+
+        [Required]
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Kod pocztowy")]
+        public string PostCode { get; set; }
+
+        [Required]
+        [Display(Name = "Ulica")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Numer domu/mieszkania")]
+        public string NumberOfApartment { get; set; }
+
+        [Display(Name = "Instruktor")]
+        public ICollection<string> Instructor { get; set; }
+    }
+}
