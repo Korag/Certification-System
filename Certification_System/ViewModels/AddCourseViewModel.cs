2ea5a7d7af4516a77ebec587ce72291e41a894d0
Łukasz Czepielik <koragrex@gmail.com>
2019-04-24 01:32:44 +0200

    Added AddCourseViewModel.

---
+++ Certification_System/ViewModels/AddCourseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Certification_System.ViewModels
+{
+    public class AddCourseViewModel
+    {
+        [Required]
+        [Display(Name = "Identyfikator")]
+        public string CourseIdentificator { get; set; }
+
+        [Required]
+        [Display(Name = "Nazwa kursu")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Data rozpoczęcia")]
+        public DateTime DateOfStart { get; set; }
+
+        [Display(Name = "Data zakończenia")]
+        public DateTime DateOfEnd { get; set; }
+
+
+        [Required(ErrorMessage = "Należy zaznaczyć conajmniej jeden Obszar.")]
+        public ICollection<string> SelectedBranches { get; set; }
+
+        [Display(Name = "Obszar")]
+        public IList<SelectListItem> AvailableBranches { get; set; }
+
+
+        public ICollection<AddMeetingViewModel> MeetingsViewModels { get; set; }
+    }
+}
