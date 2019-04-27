6be9b19a799ae3003883446fa2da38e06799d563
Łukasz Czepielik <koragrex@gmail.com>
2019-04-27 18:38:28 +0200

    Added to CompaniesController basic methods such: AddNewCompany [GET], AddNewCompany [POST] and AddNewCompanyConfirmation [GET].

--- Certification_System/Controllers/CompaniesController.cs
+++ Certification_System/Controllers/CompaniesController.cs
@@ -1,9 +1,7 @@
 ﻿using Certification_System.DAL;
+using Certification_System.Models;
 using Certification_System.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Certification_System.Controllers
@@ -18,6 +16,7 @@ namespace Certification_System.Controllers
         }
 
         // GET: DisplayAllCompanies
+        [Authorize(Roles = "Admin")]
         public ActionResult DisplayAllCompanies()
         {
             var Companies = _context.GetCompanies();
@@ -41,6 +40,68 @@ namespace Certification_System.Controllers
             }
 
             return View(DisplayCompanies);
+        }
+
+
+        // GET: AddNewCompany
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddNewCompany()
+        {
+            return View();
+        }
+
+        // GET: AddNewCompanyConfirmation
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddNewCompanyConfirmation(string companyName)
+        {
+            if (companyName != null)
+            {
+                Company company = _context.GetCompanyByName(companyName);
+
+                AddCompanyViewModel addedCompany = new AddCompanyViewModel
+                {
+                    CompanyName = company.CompanyName,
+                    Email = company.Email,
+                    Phone = company.Phone,
+                    Country = company.Country,
+                    City = company.City,
+                    PostCode = company.PostCode,
+                    Address = company.PostCode,
+                    NumberOfApartment = company.NumberOfApartment
+                };
+
+                return View(addedCompany);
+            }
+
+            return RedirectToAction(nameof(AddNewCompany));
+        }
+
+        // GET: AddNewCompany
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult AddNewCompany(AddCompanyViewModel newCompany)
+        {
+            if (ModelState.IsValid)
+            {
+                Company company = new Company
+                {
+                    CompanyName = newCompany.CompanyName,
+                    Email = newCompany.Email,
+                    Phone = newCompany.Phone,
+                    Country = newCompany.Country,
+                    City = newCompany.City,
+                    PostCode = newCompany.PostCode,
+                    Address = newCompany.PostCode,
+                    NumberOfApartment = newCompany.NumberOfApartment
+                };
+
+                _context.AddCompany(company);
+
+                return RedirectToAction("AddNewCompanyConfirmation", new { companyName = newCompany.CompanyName});
+            }
+
+            return View(newCompany);
         }
     }
 }
