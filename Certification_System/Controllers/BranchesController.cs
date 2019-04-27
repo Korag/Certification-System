3c46ddb226a30e08bf6070b2ed54787f7f3fc229
Łukasz Czepielik <koragrex@gmail.com>
2019-04-27 14:44:36 +0200

    Added BranchesController with function which presents all branches which exist in database and also needed View for such interaction.

---
+++ Certification_System/Controllers/BranchesController.cs
@@ -0,0 +1,53 @@
+﻿using Certification_System.DAL;
+using Certification_System.ViewModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Certification_System.Controllers
+{
+    public class BranchesController : Controller
+    {
+        private IDatabaseOperations _context;
+
+        public BranchesController()
+        {
+            _context = new MongoOperations();
+        }
+
+        // GET: DisplayAllBranches
+        [Authorize(Roles = "Admin")]
+        public ActionResult DisplayAllBranches()
+        {
+            var BranchesList = _context.GetBranches();
+
+            List<AddBranchViewModel> existingBranches = new List<AddBranchViewModel>();
+
+            foreach (var branch in BranchesList)
+            {
+                existingBranches.Add(new AddBranchViewModel
+                {
+                    Name = branch.Name
+                });
+            }
+
+            return View(existingBranches);
+        }
+
+        // GET: AddNewBranch
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: AddNewBranch
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult AddNewBranch()
+        {
+            return View();
+        }
+
+    }
+}
