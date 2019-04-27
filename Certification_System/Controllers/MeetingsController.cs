d56bf690651528ec3562512f7197f2272ecae7b8
Łukasz Czepielik <koragrex@gmail.com>
2019-04-27 13:18:24 +0200

    Added MeetingController with AddNewMeetingPartial View - it is not functional. In future it will be used to simulatenously add course and meetings linked with them.

---
+++ Certification_System/Controllers/MeetingsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Certification_System.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        // GET: AddNewMeetingPartial
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddNewMeetingPartial(int MeetingIdentificator)
+        {
+            ViewBag.Id = MeetingIdentificator;
+            return PartialView();
+        }
+    }
+}
