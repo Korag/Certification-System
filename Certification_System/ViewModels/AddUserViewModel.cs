6ba207c87bf6a3da66318bd2194015d501ce3b7b
Łukasz Czepielik <koragrex@gmail.com>
2019-04-29 01:09:25 +0200

    Added AddUserViewModel.

---
+++ Certification_System/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using CompareAttribute = System.Web.Mvc.CompareAttribute;
+
+namespace Certification_System.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Adres email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Hasło musi mieć długość conajmniej {2} znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Wprowadzone hasła różnią się.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Imię powinno zawierać od 3 do 100 znaków.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwisko powinno składać się od 3 do 100 znaków")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa kraju powinna mieć długość pomiędzy 3 a 100 znakami")]
+        [Display(Name = "Kraj")]
+        public string Country { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa miasta powinna mieć długość pomiędzy 3 a 100 znakami")]
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
+        [Display(Name = "Kod pocztowy")]
+        public string PostCode { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa ulicy powinna mieć długość pomiędzy 3 a 100 znakami")]
+        [Display(Name = "Ulica")]
+        public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(10, ErrorMessage = "Numer domu/mieszkania nie powinien być większy niż 10 znaków")]
+        [Display(Name = "Numer domu/mieszkania")]
+        public string NumberOfApartment { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        [Display(Name = "Numer telefonu")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [Display(Name = "Data urodzenia")]
+        public string DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Rola użytkownika")]
+        public IList<SelectListItem> AvailableRoles { get; set; }
+
+        [Required(ErrorMessage = "Należy przydzielić rolę")]
+        public string SelectedRole { get; set; }
+
+        [Required]
+        [Display(Name = "Przedsiębiorstwa")]
+        public IList<SelectListItem> AvailableCompanies { get; set; }
+
+        [Display(Name = "Przedsiębiorstwo zarządzane przez użytkownika")]
+        public string CompanyRoleAdmin { get; set; }
+
+        [Display(Name = "Przedsiębiorstwo zrzeszające pracownika")]
+        public string CompanyRoleWorker { get; set; }
+    }
+}
