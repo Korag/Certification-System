311d8d4aea3a89b2e8c328cfa8d4bdbab8b76d23
Łukasz Czepielik <koragrex@gmail.com>
2019-04-29 01:12:18 +0200

    Added UserController with AddNewUser [GET] method.

---
+++ Certification_System/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Certification_System.DAL;
+using Certification_System.Models;
+using Certification_System.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Certification_System.Controllers
+{
+    public class UserController : Controller
+    {
+        private IDatabaseOperations _context;
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public UserController()
+        {
+            _context = new MongoOperations();
+        }
+
+        public UserController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+            _context = new MongoOperations();
+        }
+
+        // GET: DisplayAllUsers
+        [Authorize(Roles = "Admin")]
+        public ActionResult DisplayAllUsers()
+        {
+            return View();
+        }
+
+        // GET: AddNewUser
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddNewUser()
+        {
+            AddUserViewModel newUser = new AddUserViewModel
+            {
+                AvailableRoles = _context.GetRolesAsSelectList().ToList(),
+                AvailableCompanies = _context.GetCompaniesAsSelectList().ToList()
+            };
+
+            return View(newUser);
+        }
+
+ 
+    }
+}
