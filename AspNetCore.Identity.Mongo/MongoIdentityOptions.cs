8b078c82874954514642eef5ccd83cb259b40397
Łukasz Czepielik <koragrex@gmail.com>
2019-05-11 20:12:13 +0200

    Added AspNetCore.Identity.Mongo provider project.

---
+++ AspNetCore.Identity.Mongo/MongoIdentityOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCore.Identity.Mongo
+{
+	public class MongoIdentityOptions
+	{
+		public string ConnectionString { get; set; } = "mongodb://Admin:Qwer!234@ds042527.mlab.com:42527/certification_system_tests";
+        
+	    public string UsersCollection { get; set; } = "Users";
+		
+	    public string RolesCollection { get; set; } = "Roles";
+
+	    public bool UseDefaultIdentity { get; set; } = true;
+	}
+}
