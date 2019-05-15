07c8c6d8df9bc2625e1588432ff6b13c1745d3d8
Łukasz Czepielik <koragrex@gmail.com>
2019-05-15 19:38:35 +0200

    Added DisplayListOfCertificatesViewModel.

---
+++ Certification-System/ViewModels/DisplayListOfCertificatesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certification_System.ViewModels
+{
+    public class DisplayListOfCertificatesViewModel
+    {
+        [Display(Name = "Identyfikator")]
+        public string CertificateIndexer { get; set; }
+
+        [Display(Name = "Nazwa certyfikatu")]
+        public string Name { get; set; }
+
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+
+        [Display(Name = "Przedawniony")]
+        public bool Depreciated { get; set; }
+
+        [Display(Name = "Obszary")]
+        public ICollection<string> Branches { get; set; }
+    }
+}
