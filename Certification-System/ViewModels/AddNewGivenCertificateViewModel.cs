a583fbc283993292216203807ac17144d1face32
Łukasz Czepielik <koragrex@gmail.com>
2019-05-16 00:53:33 +0200

    Added AddNewGivenCertificateViewModel.

---
+++ Certification-System/ViewModels/AddNewGivenCertificateViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.ViewModels
+{
+    public class AddNewGivenCertificateViewModel
+    {
+        [Required]
+        [Display(Name = "Identyfikator nadawanego certyfikatu")]
+        public string GivenCertificateIndexer { get; set; }
+
+        [Required]
+        [Display(Name = "Data otrzymania")]
+        public DateTime ReceiptDate { get; set; }
+
+        [Required]
+        [Display(Name = "Data wygaśnięcia")]
+        public DateTime ExpirationDate { get; set; }
+
+
+        [Display(Name = "Instruktorzy")]
+        public IList<SelectListItem> AvailableCertificates { get; set; }
+
+        [Required]
+        public string SelectedCertificate { get; set; }
+
+        [Display(Name = "Użytkownicy")]
+        public IList<SelectListItem> AvailableUsers{ get; set; }
+
+        [Required]
+        public string SelectedUser { get; set; }
+
+        [Display(Name = "Szkolenia")]
+        public IList<SelectListItem> AvailableCourses { get; set; }
+
+        [Required]
+        public string SelectedCourses { get; set; }
+    }
+}
