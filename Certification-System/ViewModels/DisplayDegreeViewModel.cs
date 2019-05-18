8f784d7dd102b20c69931f896a3357d56c6564cf
Łukasz Czepielik <koragrex@gmail.com>
2019-05-18 19:28:07 +0200

    Added DisplayDegreeViewModel.

---
+++ Certification-System/ViewModels/DisplayDegreeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.ViewModels
+{
+    public class DisplayDegreeViewModel
+    {
+        [Display(Name = "Identyfikator")]
+        public string DegreeIndexer { get; set; }
+
+        [Display(Name = "Nazwa stopnia zawodowego")]
+        public string Name { get; set; }
+
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+
+        [Display(Name = "Obszary stopnia zawodowego")]
+        public ICollection<string> Branches { get; set; }
+
+        [Display(Name = "Wymagane certyfikaty")]
+        public ICollection<string> RequiredCertificates { get; set; }
+
+        [Display(Name = "Wymagane stopnie zawodowe")]
+        public ICollection<string> RequiredDegrees { get; set; }
+    }
+}
