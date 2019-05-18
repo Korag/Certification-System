9cab7b1fc71fa4aa1852f6195588644c5e56957e
Łukasz Czepielik <koragrex@gmail.com>
2019-05-18 19:29:01 +0200

    Added AddNewDegreeConfirmation [GET] action to DegreesController.

--- Certification-System/Controllers/DegreesController.cs
+++ Certification-System/Controllers/DegreesController.cs
@@ -56,6 +56,15 @@ namespace Certification_System.Controllers
                     Branches = newDegree.SelectedBranches
                 };
 
+                if (degree.RequiredDegrees == null)
+                {
+                    degree.RequiredDegrees = new List<string>();
+                }
+                if (degree.RequiredCertificates == null)
+                {
+                    degree.RequiredCertificates = new List<string>();
+                }
+
                 _context.AddDegree(degree);
 
                 return RedirectToAction("AddNewDegreeConfirmation", new { degreeIdentificator = degree.DegreeIdentificator });
@@ -67,6 +76,34 @@ namespace Certification_System.Controllers
                 newDegree.SelectedBranches = new List<string>();
             }
             return View(newDegree);
+        }
+
+        // GET: AddNewDegreeConfirmation
+        [Authorize(Roles = "Admin")]
+        public IActionResult AddNewDegreeConfirmation(string degreeIdentificator)
+        {
+            if (degreeIdentificator != null)
+            {
+                var Degree = _context.GetDegreeById(degreeIdentificator);
+
+                var RequiredCertificates = _context.GetCertificatesById(Degree.RequiredCertificates);
+                var RequiredDegrees = _context.GetDegreesById(Degree.RequiredDegrees);
+                var Branches = _context.GetBranchesById(Degree.Branches);
+
+                DisplayDegreeViewModel addedDegree = new DisplayDegreeViewModel
+                {
+                    DegreeIndexer = Degree.DegreeIndexer,
+                    Name = Degree.Name,
+                    Description = Degree.Description,
+
+                    RequiredCertificates = RequiredCertificates.Select(z => z.CertificateIndexer + " " + z.Name).ToList(),
+                    RequiredDegrees = RequiredDegrees.Select(z => z.DegreeIdentificator + " " + z.Name).ToList(),
+                    Branches = Branches
+                };
+
+                return View(addedDegree);
+            }
+            return RedirectToAction(nameof(AddNewDegree));
         }
     }
 }
