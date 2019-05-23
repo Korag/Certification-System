f0b9a2a5b60a598e587bb6e55432b7cc543622f5
Łukasz Czepielik <koragrex@gmail.com>
2019-05-23 11:19:22 +0200

    Added DisplayMeetingViewModel, DisplayListOfCoursesViewModel and CourseDetailsViewModel.

---
+++ Certification-System/ViewModels/DisplayMeetingViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.ViewModels
+{
+    public class DisplayMeetingViewModel
+    {
+        public string CourseIdentificator { get; set; }
+
+        public string MeetingIdentificator { get; set; }
+
+        [Display(Name = "Identyfikator")]
+        public string MeetingIndexer { get; set; }
+
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+
+        [Display(Name = "Data szkolenia")]
+        public DateTime DateOfMeeting { get; set; }
+
+        [Display(Name = "Państwo")]
+        public string Country { get; set; }
+
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
+        public string PostCode { get; set; }
+
+        [Display(Name = "Ulica")]
+        public string Address { get; set; }
+
+        [Display(Name = "Numer domu/mieszkania")]
+        public string NumberOfApartment { get; set; }
+
+        [Display(Name = "Instruktorzy")]
+        public ICollection<string> Instructors { get; set; }
+    }
+}
