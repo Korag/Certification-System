ff89ba8161feabb6b64148c50c7ff54d7dcdcdce
Łukasz Czepielik <koragrex@gmail.com>
2019-05-23 11:23:52 +0200

    Small changes in DisplayUsersViewModel.

--- Certification-System/ViewModels/DisplayUsersViewModel.cs
+++ Certification-System/ViewModels/DisplayUsersViewModel.cs
@@ -5,25 +5,18 @@ namespace Certification_System.ViewModels
 {
     public class DisplayUsersViewModel
     {
-        [Required]
-        [EmailAddress]
+        public string UserIdentificator { get; set; }
+
         [Display(Name = "Adres email")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Imię powinno zawierać od 3 do 100 znaków.")]
-        [DataType(DataType.Text)]
         [Display(Name = "Imię")]
         public string FirstName { get; set; }
 
-        [Required]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwisko powinno składać się od 3 do 100 znaków")]
-        [DataType(DataType.Text)]
         [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
 
         [Display(Name = "Role użytkownika")]
-        [Required(ErrorMessage = "Należy przydzielić rolę")]
         public ICollection<string> Roles { get; set; }
 
 
