0f31623ce8a4d89c3606b782df39fb1f2b872828
Łukasz Czepielik <koragrex@gmail.com>
2019-06-05 19:31:03 +0200

    Added VerifyCertificate View with appropriate ViewModel.

---
+++ Certification-System/ViewModels/VerifyCertificateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.ViewModels
+{
+    public class VerifyCertificateViewModel
+    {
+        [Display(Name = "Identyfikator nadanego certyfikatu")]
+        public string CertificateIdentificator { get; set; }
+    }
+}
