3b1f98ea7102b4c1daa45c01141076ff38d3886b
Łukasz Czepielik <koragrex@gmail.com>
2019-06-05 19:30:36 +0200

    Added VerifyUser View with appropriate ViewModel.

---
+++ Certification-System/ViewModels/VerifyUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.ViewModels
+{
+    public class VerifyUserViewModel
+    {
+        [Display(Name="Identyfikator użytkownika")]
+        public string UserIdentificator { get; set; }
+    }
+}
