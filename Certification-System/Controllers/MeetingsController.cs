7f866fc03346fd0ff50d08d10008143bce44f35a
Łukasz Czepielik <koragrex@gmail.com>
2019-08-04 23:38:47 +0200

    Refactored MeetingsController by added AutoMapper mappings. Changed some used ViewModels types. Fixed small bugs.

--- Certification-System/Controllers/MeetingsController.cs
+++ Certification-System/Controllers/MeetingsController.cs
@@ -2,53 +2,47 @@
 using Certification_System.DTOViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Bson;
 using System.Collections.Generic;
 using System.Linq;
 using Certification_System.Repository.DAL;
+using Certification_System.ServicesInterfaces;
+using AutoMapper;
 
 namespace Certification_System.Controllers
 {
     public class MeetingsController : Controller
     {
-        public MongoOperations _context { get; set; }
+        private readonly MongoOperations _context;
 
-        public MeetingsController(MongoOperations context)
+        private readonly IMapper _mapper;
+        private readonly IKeyGenerator _keyGenerator;
+
+        public MeetingsController(MongoOperations context, IMapper mapper, IKeyGenerator keyGenerator)
         {
             _context = context;
+            _mapper = mapper;
+            _keyGenerator = keyGenerator;
         }
 
-        // GET: AddNewMeetingConfirmation
+        // GET: ConfirmationOfActionOnMeeting
         [Authorize(Roles = "Admin")]
-        public IActionResult AddNewMeetingConfirmation(string meetingIdentificator)
+        public ActionResult ConfirmationOfActionOnMeeting(string meetingIdentificator)
         {
             if (meetingIdentificator != null)
             {
                 var Meeting = _context.meetingRepository.GetMeetingById(meetingIdentificator);
 
-                AddMeetingViewModel addedMeeting = new AddMeetingViewModel
-                {
-                    MeetingIdentificator = Meeting.MeetingIdentificator,
-
-                    SelectedCourse = _context.courseRepository.GetCourseByMeetingId(meetingIdentificator).CourseIndexer,
-                    MeetingIndexer = Meeting.MeetingIndexer,
-                    Description = Meeting.Description,
-                    DateOfMeeting = Meeting.DateOfMeeting,
-                    Country = Meeting.Country,
-                    City = Meeting.City,
-                    PostCode = Meeting.PostCode,
-                    Address = Meeting.Address,
-                    NumberOfApartment = Meeting.NumberOfApartment,
-                    SelectedInstructors = new List<string>()
-                };
-
-                if (addedMeeting.SelectedInstructors != null)
+                DisplayMeetingViewModel modifiedMeeting = _mapper.Map<DisplayMeetingViewModel>(Meeting);
+                modifiedMeeting.CourseIndexer = _context.courseRepository.GetCourseByMeetingId(meetingIdentificator).CourseIndexer;
+                modifiedMeeting.InstructorsCredentials = new List<string>();
+          
+                if (Meeting.Instructors != null)
                 {
                     var Instructors = _context.instructorRepository.GetInstructorsById(Meeting.Instructors);
-                    addedMeeting.SelectedInstructors = Instructors.Select(z => z.FirstName + " " + z.LastName).ToList();
+                    modifiedMeeting.InstructorsCredentials = Instructors.Select(z => z.FirstName + " " + z.LastName).ToList();
                 }
 
-                return View(addedMeeting);
+                return View(modifiedMeeting);
             }
 
             return RedirectToAction(nameof(AddNewMeeting));
@@ -57,14 +51,14 @@ namespace Certification_System.Controllers
         // LEGACY
         // GET: AddNewMeetingPartial
         [Authorize(Roles = "Admin")]
-        public IActionResult AddNewMeetingPartial()
+        public ActionResult AddNewMeetingPartial()
         {
             return PartialView();
         }
 
         // GET: AddNewMeeting
         [Authorize(Roles = "Admin")]
-        public IActionResult AddNewMeeting()
+        public ActionResult AddNewMeeting()
         {
             AddMeetingViewModel newMeeting = new AddMeetingViewModel
             {
@@ -76,40 +70,26 @@ namespace Certification_System.Controllers
         }
 
         // POST: AddNewMeeting
-        [ValidateAntiForgeryToken]
         [Authorize(Roles = "Admin")]
         [HttpPost]
-        public IActionResult AddNewMeeting(AddMeetingViewModel newMeeting)
+        public ActionResult AddNewMeeting(AddMeetingViewModel newMeeting)
         {
             if (ModelState.IsValid)
             {
-                Meeting meeting = new Meeting
-                {
-                    MeetingIdentificator = ObjectId.GenerateNewId().ToString(),
-
-                    MeetingIndexer = newMeeting.MeetingIndexer,
-                    Description = newMeeting.Description,
-                    DateOfMeeting = newMeeting.DateOfMeeting,
-                    Country = newMeeting.Country,
-                    City = newMeeting.City,
-                    PostCode = newMeeting.PostCode,
-                    Address = newMeeting.Address,
-                    NumberOfApartment = newMeeting.NumberOfApartment,
-
-                    Instructors = newMeeting.SelectedInstructors
-                };
+                Meeting meeting = _mapper.Map<Meeting>(newMeeting);
+                meeting.MeetingIdentificator = _keyGenerator.GenerateNewId();
 
                 _context.meetingRepository.AddMeeting(meeting);
                 _context.courseRepository.AddMeetingToCourse(meeting.MeetingIdentificator, newMeeting.SelectedCourse);
 
-                return RedirectToAction("AddNewMeetingConfirmation", new { meetingIdentificator = meeting.MeetingIdentificator, TypeOfAction = "Add" });
+                return RedirectToAction("ConfirmationOfActionOnMeeting", new { meetingIdentificator = meeting.MeetingIdentificator, TypeOfAction = "Add" });
             }
 
             newMeeting.AvailableCourses = _context.courseRepository.GetCoursesAsSelectList().ToList();
             newMeeting.AvailableInstructors = _context.instructorRepository.GetInstructorsAsSelectList().ToList();
+
             return View(newMeeting);
         }
-
 
         // GET: EditMeeting
         [Authorize(Roles = "Admin")]
@@ -117,28 +97,13 @@ namespace Certification_System.Controllers
         {
             var Meeting = _context.meetingRepository.GetMeetingById(meetingIdentificator);
 
-            EditMeetingViewModel meetingViewModel = new EditMeetingViewModel
-            {
-                MeetingIdentificator = Meeting.MeetingIdentificator,
-
-                MeetingIndexer = Meeting.MeetingIndexer,
-                Description = Meeting.Description,
-                DateOfMeeting = Meeting.DateOfMeeting,
-                Country = Meeting.Country,
-                City = Meeting.City,
-                Address = Meeting.Address,
-                NumberOfApartment = Meeting.NumberOfApartment,
-                PostCode = Meeting.PostCode,
-
-                AvailableInstructors = _context.instructorRepository.GetInstructorsAsSelectList().ToList(),
-                SelectedInstructors = Meeting.Instructors
-            };
+            EditMeetingViewModel meetingViewModel = _mapper.Map<EditMeetingViewModel>(Meeting);
+            meetingViewModel.AvailableInstructors = _context.instructorRepository.GetInstructorsAsSelectList().ToList();
 
             return View(meetingViewModel);
         }
 
         // POST: EditMeeting
-        [ValidateAntiForgeryToken]
         [Authorize(Roles = "Admin")]
         [HttpPost]
         public ActionResult EditMeeting(EditMeetingViewModel editedMeeting)
@@ -146,20 +111,11 @@ namespace Certification_System.Controllers
             if (ModelState.IsValid)
             {
                 var OriginMeeting = _context.meetingRepository.GetMeetingById(editedMeeting.MeetingIdentificator);
-
-                OriginMeeting.MeetingIndexer = editedMeeting.MeetingIndexer;
-                OriginMeeting.Description = editedMeeting.Description;
-                OriginMeeting.DateOfMeeting = editedMeeting.DateOfMeeting;
-                OriginMeeting.Country = editedMeeting.Country;
-                OriginMeeting.City = editedMeeting.City;
-                OriginMeeting.PostCode = editedMeeting.PostCode;
-                OriginMeeting.NumberOfApartment = editedMeeting.NumberOfApartment;
-                OriginMeeting.Address = editedMeeting.Address;
-                OriginMeeting.Instructors = editedMeeting.SelectedInstructors;
+                OriginMeeting = _mapper.Map<EditMeetingViewModel, Meeting>(editedMeeting, OriginMeeting);
 
                 _context.meetingRepository.UpdateMeeting(OriginMeeting);
 
-                return RedirectToAction("AddNewMeetingConfirmation", "Meetings", new { meetingIdentificator = editedMeeting.MeetingIdentificator, TypeOfAction = "Update" });
+                return RedirectToAction("ConfirmationOfActionOnMeeting", "Meetings", new { meetingIdentificator = editedMeeting.MeetingIdentificator, TypeOfAction = "Update" });
             }
 
             return View(editedMeeting);
@@ -177,26 +133,13 @@ namespace Certification_System.Controllers
                 Course RelatedCourse = _context.courseRepository.GetCourseByMeetingId(meeting.MeetingIdentificator);
                 List<Instructor> RelatedInstructors = _context.instructorRepository.GetInstructorsById(meeting.Instructors).ToList();
 
-                DisplayMeetingViewModel singleMeeting = new DisplayMeetingViewModel
-                {
-                    MeetingIdentificator = meeting.MeetingIdentificator,
+                DisplayMeetingViewModel singleMeeting = _mapper.Map<DisplayMeetingViewModel>(meeting);
+                singleMeeting.CourseIdentificator = RelatedCourse.CourseIdentificator;
+                singleMeeting.CourseIndexer = RelatedCourse.CourseIndexer;
+                singleMeeting.CourseName = RelatedCourse.Name;
 
-                    MeetingIndexer = meeting.MeetingIndexer,
-                    Description = meeting.Description,
-                    DateOfMeeting = meeting.DateOfMeeting,
-                    Country = meeting.Country,
-                    City = meeting.City,
-                    Address = meeting.Address,
-                    NumberOfApartment = meeting.NumberOfApartment,
-                    PostCode = meeting.PostCode,
-
-                    CourseIdentificator = RelatedCourse.CourseIdentificator,
-                    CourseIndexer = RelatedCourse.CourseIndexer,
-                    CourseName = RelatedCourse.Name,
-
-                    InstructorsIdentificators = RelatedInstructors.Select(z => z.InstructorIdentificator).ToList(),
-                    InstructorsCredentials = RelatedInstructors.Select(z => z.FirstName + " " + z.LastName).ToList()
-                };
+                singleMeeting.InstructorsIdentificators = RelatedInstructors.Select(z => z.InstructorIdentificator).ToList();
+                singleMeeting.InstructorsCredentials = RelatedInstructors.Select(z => z.FirstName + " " + z.LastName).ToList();
 
                 ListOfMeetings.Add(singleMeeting);
             }
@@ -210,68 +153,28 @@ namespace Certification_System.Controllers
         {
             var Meeting = _context.meetingRepository.GetMeetingById(meetingIdentificator);
             var RelatedInstructors = _context.instructorRepository.GetInstructorsById(Meeting.Instructors);
+            
             var EnrolledUsersIdentificators = _context.courseRepository.GetCourseByMeetingId(meetingIdentificator).EnrolledUsers;
-
             var EnrolledUsersList = _context.userRepository.GetUsersById(EnrolledUsersIdentificators);
 
-            List<AddInstructorViewModel> ListOfInstructors = new List<AddInstructorViewModel>();
+            List<DisplayCrucialDataWithContactUsersViewModel> ListOfInstructors = new List<DisplayCrucialDataWithContactUsersViewModel>();
 
             if (RelatedInstructors.Count != 0)
             {
-                foreach (var instructor in RelatedInstructors)
-                {
-                    AddInstructorViewModel singleInstructor = new AddInstructorViewModel
-                    {
-                        FirstName = instructor.FirstName,
-                        LastName = instructor.LastName,
-                        Email = instructor.Email,
-                        Phone = instructor.Phone,
-                        Country = instructor.Country,
-                        City = instructor.City,
-                        PostCode = instructor.PostCode,
-                        Address = instructor.Address,
-                        NumberOfApartment = instructor.NumberOfApartment
-                    };
-
-                    ListOfInstructors.Add(singleInstructor);
-                }
+               ListOfInstructors = _mapper.Map<List<DisplayCrucialDataWithContactUsersViewModel>>(RelatedInstructors);
             }
 
-            List<DisplayUsersViewModel> ListOfUsers = new List<DisplayUsersViewModel>();
+            List<DisplayCrucialDataUsersViewModel> ListOfUsers = new List<DisplayCrucialDataUsersViewModel>();
 
             if (EnrolledUsersList.Count != 0)
             {
-                foreach (var user in EnrolledUsersList)
-                {
-                    DisplayUsersViewModel singleUser = new DisplayUsersViewModel
-                    {
-                        UserIdentificator = user.Id,
-                        FirstName = user.FirstName,
-                        LastName = user.LastName,
-                        Email = user.Email,
-                    };
-
-                    ListOfUsers.Add(singleUser);
-                }
+                ListOfUsers = _mapper.Map<List<DisplayCrucialDataUsersViewModel>>(EnrolledUsersList);
             }
 
-            MeetingDetailsViewModel MeetingDetails = new MeetingDetailsViewModel
-            {
-                MeetingIdentificator = Meeting.MeetingIdentificator,
-
-                MeetingIndexer = Meeting.MeetingIndexer,
-                Description = Meeting.Description,
-                DateOfMeeting = Meeting.DateOfMeeting,
-                Country = Meeting.Country,
-                City = Meeting.City,
-                Address = Meeting.Address,
-                NumberOfApartment = Meeting.NumberOfApartment,
-                PostCode = Meeting.PostCode,
-
-                Instructors = ListOfInstructors,
-                AttendanceList = Meeting.AttendanceList,
-                AllCourseParticipants = ListOfUsers
-            };
+            MeetingDetailsViewModel MeetingDetails = _mapper.Map<MeetingDetailsViewModel>(Meeting);
+            MeetingDetails.Instructors = ListOfInstructors;
+            MeetingDetails.AttendanceList = Meeting.AttendanceList;
+            MeetingDetails.AllCourseParticipants = ListOfUsers;
 
             return View(MeetingDetails);
         }
