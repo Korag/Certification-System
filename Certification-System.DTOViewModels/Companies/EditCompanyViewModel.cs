1bfdada023ce7d0c9c2d9d0b567f33a68fe1d8bb
Łukasz Czepielik <koragrex@gmail.com>
2019-08-04 23:32:21 +0200

    Added new ViewModels based on Company entity.

---
+++ Certification-System.DTOViewModels/Companies/EditCompanyViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class EditCompanyViewModel
+    {
+        public string CompanyIdentificator { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [Display(Name = "Nazwa przedsiębiorstwa")]
+        [MaxLength(100)]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [EmailAddress(ErrorMessage = "W polu \"{0}\" nie znajduje się poprawny adres email.")]
+        [Display(Name = "Adres email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        [Display(Name = "Numer telefonu")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa kraju powinna mieć długość pomiędzy 3 a 100 znakami")]
+        [Display(Name = "Kraj")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa miasta powinna mieć długość pomiędzy 3 a 100 znakami")]
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [DataType(DataType.PostalCode)]
+        [Display(Name = "Kod pocztowy")]
+        public string PostCode { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa ulicy powinna mieć długość pomiędzy 3 a 100 znakami")]
+        [Display(Name = "Ulica")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [DataType(DataType.Text)]
+        [StringLength(10, ErrorMessage = "Numer domu/mieszkania nie powinien być większy niż 10 znaków")]
+        [Display(Name = "Numer domu/mieszkania")]
+        public string NumberOfApartment { get; set; }
+    }
+}
