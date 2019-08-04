1bc6123a6422dbe88adbf04626f878a6f6d5236a
Łukasz Czepielik <koragrex@gmail.com>
2019-08-04 23:30:46 +0200

    Added new ViewModels based on GivenCertificate entity.

---
+++ Certification-System.DTOViewModels/GivenCertificates/DisplayGivenCertificateToUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Certification_System.DTOViewModels;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayGivenCertificateToUserViewModel
+    {
+        public string GivenCertificateIdentificator { get; set; }
+
+        [Display(Name = "Identyfikator nadanego certyfikatu")]
+        public string GivenCertificateIndexer { get; set; }
+
+        [Display(Name = "Data otrzymania")]
+        public DateTime ReceiptDate { get; set; }
+
+        [Display(Name = "Data wygaśnięcia")]
+        public DateTime ExpirationDate { get; set; }
+
+        [Display(Name = "Nadany certyfikat")]
+        public DisplayCrucialDataCertificateViewModel Certificate { get; set; }
+
+        [Display(Name = "Szkolenie")]
+        public DisplayCrucialDataCourseViewModel Course { get; set; }
+    }
+}
