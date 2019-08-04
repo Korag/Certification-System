31c8bb48acd4a57561b6580c232c09a0fe213b59
Łukasz Czepielik <koragrex@gmail.com>
2019-08-04 23:32:02 +0200

    Added new ViewModels based on Course entity.

---
+++ Certification-System.DTOViewModels/Courses/DisplayCrucialDataCourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayCrucialDataCourseViewModel
+    {
+        public string CourseIdentificator { get; set; }
+
+        [Display(Name = "Identyfikator")]
+        public string CourseIndexer { get; set; }
+
+        [Display(Name = "Nazwa kursu")]
+        public string Name { get; set; }
+
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+    }
+}
