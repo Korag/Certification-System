56df13c77d624291cb008b42e07c7415e673e3c0
Łukasz Czepielik <koragrex@gmail.com>
2019-08-04 23:32:52 +0200

    Added new ViewModels based on Certificate entity.

---
+++ Certification-System.DTOViewModels/Certificates/DisplayCrucialDataCertificateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayCrucialDataCertificateViewModel
+    {
+        public string CertificateIdentificator{ get; set; }
+
+        [Display(Name = "Identyfikator")]
+        public string CertificateIndexer { get; set; }
+
+        [Display(Name = "Nazwa certyfikatu")]
+        public string Name { get; set; }
+    }
+}
---
+++ Certification-System.DTOViewModels/Certificates/EditCertificateViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class EditCertificateViewModel
+    {
+        public string CertificateIdentificator { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [Display(Name="Identyfikator")]
+        public string CertificateIndexer { get; set; }
+
+        [Required(ErrorMessage = "Pole \"{0}\" jest wymagane.")]
+        [StringLength(100, ErrorMessage = "Pole \"{0}\" musi mieć długość conajmniej {2} znaków.", MinimumLength = 6)]
+        [Display(Name = "Nazwa certyfikatu")]
+        public string Name { get; set; }
+
+        [Display(Name = "Opis")]
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        [Display(Name = "Przedawniony")]
+        public bool Depreciated { get; set; }
+
+        [Required(ErrorMessage = "Należy zaznaczyć conajmniej jeden Obszar.")]
+        public ICollection<string> SelectedBranches{ get; set; }
+
+        [Display(Name = "Obszar")]
+        public IList<SelectListItem> AvailableBranches { get; set; }
+    }
+}
