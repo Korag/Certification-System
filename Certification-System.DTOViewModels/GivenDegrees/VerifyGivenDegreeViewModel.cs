0a2001f72365e36a64db172250c59ff5bbe94d9d
Łukasz Czepielik <koragrex@gmail.com>
2019-08-11 01:56:46 +0200

    Added new card about verifying GivenDegree to VerifyCompetenceManual View and also added form with user crucial data to AnonymousVerificationOfGivenCertificate View.

--- Certification-System.DTOViewModels/GivenDegrees/VerifyGivenDegreeViewModel.cs
+++ Certification-System.DTOViewModels/GivenDegrees/VerifyGivenDegreeViewModel.cs
@@ -4,7 +4,7 @@ namespace Certification_System.DTOViewModels
 {
     public class VerifyGivenDegreeViewModel
     {
-        [Display(Name = "Identyfikator nadanego certyfikatu")]
+        [Display(Name = "Identyfikator nadanego stopnia zawodowego")]
         public string GivenDegreeIdentificator { get; set; }
 
         public bool GivenDegreeIdentificatorNotExist { get; set; }
