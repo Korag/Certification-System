a57ad329015c2cb2723ef9559d9da301302e56e9
Łukasz Czepielik <koragrex@gmail.com>
2019-08-21 19:56:53 +0200

    Added AddNewExam [GET] and [POST] action to ExamsController. Added new entries to _AdminMenu and designed idea of work for AddExamTermViewComponent.

---
+++ Certification-System/Components/AddExamTermViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Certification_System.Components
+{
+    public class AddExamTermViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string orderNumber)
+        {
+            return View("_AddExamTerm", orderNumber);
+        }
+    }
+}
--- Certification-System/Controllers/ExamsController.cs
+++ Certification-System/Controllers/ExamsController.cs
@@ -1,7 +1,12 @@
 ﻿using AutoMapper;
+using Certification_System.DTOViewModels;
+using Certification_System.Entities;
 using Certification_System.Repository.DAL;
 using Certification_System.ServicesInterfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Certification_System.Controllers
 {
@@ -18,5 +23,79 @@ namespace Certification_System.Controllers
             _mapper = mapper;
             _keyGenerator = keyGenerator;
         }
+
+        // GET: AddNewExam
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddNewExam(string courseIdentificator)
+        {
+            AddExamViewModel newExam = new AddExamViewModel
+            {
+                AvailableCourses = _context.courseRepository.GetActiveCoursesAsSelectList().ToList(),
+                AvailableExaminers = _context.userRepository.GetExaminersAsSelectList().ToList(),
+                AvailableExamTerms = new List<AddExamTermViewModel>()
+            };
+
+            if (!string.IsNullOrWhiteSpace(courseIdentificator))
+            {
+                newExam.SelectedCourse = courseIdentificator;
+            }
+
+            return View(newExam);
+        }
+
+        // POST: AddNewExam
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult AddNewExam(AddExamViewModel newExam)
+        {
+            if (ModelState.IsValid)
+            {
+                var course = _context.courseRepository.GetCourseById(newExam.SelectedCourse);
+    
+                Exam exam = _mapper.Map<Exam>(newExam);
+                exam.ExamIdentificator = _keyGenerator.GenerateNewId();
+
+                course.Exams.Add(exam.ExamIdentificator);
+
+                exam.DurationDays = (int)exam.DateOfEnd.Subtract(exam.DateOfStart).TotalDays;
+                exam.DurationMinutes = (int)exam.DateOfEnd.Subtract(exam.DateOfStart).TotalMinutes;
+                exam.OrdinalNumber = course.Exams.Count();
+
+                List<ExamTerm> examsTerms = new List<ExamTerm>();
+
+                if (exam.AvailableExamTerms.Count() != 0)
+                {
+                    foreach (var newExamTerm in newExam.AvailableExamTerms)
+                    {
+                        ExamTerm examTerm = _mapper.Map<ExamTerm>(newExamTerm);
+                        examTerm.ExamTermIdentificator = _keyGenerator.GenerateNewId();
+
+                        examTerm.DurationDays = (int)newExamTerm.DateOfEnd.Subtract(newExamTerm.DateOfStart).TotalDays;
+                        examTerm.DurationMinutes = (int)newExamTerm.DateOfEnd.Subtract(newExamTerm.DateOfStart).TotalMinutes;
+
+                        exam.AvailableExamTerms.Add(examTerm.ExamTermIdentificator);
+                        examsTerms.Add(examTerm);
+                    }
+                }
+
+                _context.examRepository.AddExam(exam);
+                _context.examTermRepository.AddExamsTerms(examsTerms);
+
+                return RedirectToAction("ConfirmationOfActionOnExam", new { examIdentificator = exam.ExamIdentificator, TypeOfAction = "Add" });
+            }
+
+            newExam.AvailableCourses = _context.courseRepository.GetActiveCoursesAsSelectList().ToList();
+            newExam.AvailableExaminers = _context.userRepository.GetExaminersAsSelectList().ToList();
+
+            return View(newExam);
+        }
+
+        // GET: GetAddExamTermViewComponent
+        [Authorize(Roles = "Admin")]
+        public ActionResult GetAddExamTermViewComponent(string orderNumber)
+        {
+            return ViewComponent("AddExamTerm", new { orderNumber = orderNumber });
+        }
     }
 }
+
