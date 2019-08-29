eba5f04b056df37f11995742cb16bc79fa383e0f
Łukasz Czepielik <koragrex@gmail.com>
2019-08-29 18:28:05 +0200

    Added Modal as ViewComponent.

---
+++ Certification-System.DTOViewModels/Helpers/ModalViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class ModalViewModel
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public string Action { get; set; }
+        public string Controller { get; set; }
+
+        public string ButtonText { get; set; }
+        public string ButtonClass { get; set; }
+    }
+}
---
+++ Certification-System/Components/ModalViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Certification_System.Components
+{
+    public class ModalViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string title, string body, string action, string controller, string buttonText, string buttonClass)
+        {
+            ModalViewModel modal = new ModalViewModel
+            {
+                Title = title,
+                Body = body,
+
+                Action = action,
+                Controller = controller,
+
+                ButtonText = buttonText,
+                ButtonClass = buttonClass
+            };
+
+            return View("_Modal", modal);
+        }
+    }
+}
