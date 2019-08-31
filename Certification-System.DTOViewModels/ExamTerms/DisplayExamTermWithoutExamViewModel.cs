4613a941a0d10197e4b4d66188f344f7977fa05d
Łukasz Czepielik <koragrex@gmail.com>
2019-08-31 19:36:48 +0200

    Added DisplayExamTermDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ExamTerms/DisplayExamTermWithoutExamViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayExamTermWithoutExamViewModel
+    {
+        public string ExamTermIdentificator { get; set; }
+
+        [Display(Name = "Data rozpoczęcia")]
+        public DateTime DateOfStart { get; set; }
+
+        [Display(Name = "Data zakończenia")]
+        public DateTime DateOfEnd { get; set; }
+
+        [Display(Name = "Czas [dni]")]
+        public DateTime DurationDays { get; set; }
+
+        [Display(Name = "Czas [min]")]
+        public DateTime DurationMinutes { get; set; }
+
+        [Display(Name = "Liczba uczestników")]
+        public int UsersQuantitiy { get; set; }
+
+        [Display(Name = "Limit uczestników")]
+        public int UsersLimit { get; set; }
+
+        [Display(Name = "Egzaminatorzy")]
+        public ICollection<DisplayCrucialDataUserViewModel> Examiners { get; set; }
+    }
+}
---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayExamTermDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayExamTermDataTableViewModel
+    {
+        public ICollection<DisplayExamTermViewModel> ExamsTerms{ get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
