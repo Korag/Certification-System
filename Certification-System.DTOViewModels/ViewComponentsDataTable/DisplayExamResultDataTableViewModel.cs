b330353f160b706b1b8a790c8609d9cdb3eccc11
Łukasz Czepielik <koragrex@gmail.com>
2019-08-31 19:17:30 +0200

    Added DisplayExamResultDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayExamResultDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayExamResultDataTableViewModel
+    {
+        public ICollection<DisplayExamResultViewModel> ExamsResults { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/ExamsResults/DisplayExamResultDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayExamResultDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayExamResultViewModel> examResultViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayExamResultDataTableViewModel examResultDataTableViewModel = new DisplayExamResultDataTableViewModel
+            {
+                ExamsResults = examResultViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayExamResultDataTable", examResultDataTableViewModel);
+        }
+    }
+}
