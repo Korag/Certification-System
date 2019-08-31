10f97e8ef337df3bc278ca714e8e5acec4e7567c
Łukasz Czepielik <koragrex@gmail.com>
2019-09-01 01:06:35 +0200

    Added DisplayMeetingWithoutCourseDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayMeetingWithoutCourseDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayMeetingWithoutCourseDataTableViewModel
+    {
+        public ICollection<DisplayMeetingWithoutCourseViewModel> Meetings { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Meetings/DisplayMeetingWithoutCourseDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayMeetingWithoutCourseDataTableViewComponen : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayMeetingWithoutCourseViewModel> meetingViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayMeetingWithoutCourseDataTableViewModel meetingDataTableViewModel = new DisplayMeetingWithoutCourseDataTableViewModel
+            {
+                Meetings = meetingViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayMeetingWithoutCourseDataTable", meetingDataTableViewModel);
+        }
+    }
+}
