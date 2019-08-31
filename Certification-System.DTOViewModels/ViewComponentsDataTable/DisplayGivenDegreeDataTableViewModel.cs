5e152971d27893986453bf75c0a52144dfea91b5
Łukasz Czepielik <koragrex@gmail.com>
2019-08-31 23:22:33 +0200

    Added DisplayGivenDegreeDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayGivenDegreeDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayGivenDegreeDataTableViewModel
+    {
+        public ICollection<DisplayGivenDegreeViewModel> GivenDegrees { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/GivenDegrees/DisplayGivenDegreeDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayGivenDegreeDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayGivenDegreeViewModel> givenDegreeViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayGivenDegreeDataTableViewModel givenDegreeDataTableViewModel = new DisplayGivenDegreeDataTableViewModel
+            {
+                GivenDegrees = givenDegreeViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayMeetingDataTable", givenDegreeDataTableViewModel);
+        }
+    }
+}
