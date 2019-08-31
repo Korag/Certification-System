5ec83b6021cb80955b7a97ad9aa51f29ec3809e3
Łukasz Czepielik <koragrex@gmail.com>
2019-08-31 22:51:45 +0200

    Added DisplayMeetingDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayMeetingDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayMeetingDataTableViewModel
+    {
+        public ICollection<DisplayMeetingViewModel> Meetings { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Meetings/DisplayMeetingDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayMeetingDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayMeetingViewModel> meetingViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayMeetingDataTableViewModel meetingDataTableViewModel = new DisplayMeetingDataTableViewModel
+            {
+                Meetings = meetingViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayMeetingDataTable", meetingDataTableViewModel);
+        }
+    }
+}
