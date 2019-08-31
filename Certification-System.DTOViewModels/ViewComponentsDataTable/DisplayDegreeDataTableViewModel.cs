79f64a4223723c0c8a64d506d1ebdc9a5bad22a0
Łukasz Czepielik <koragrex@gmail.com>
2019-08-31 18:17:59 +0200

    Added DisplayDegreeDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayDegreeDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayDegreeDataTableViewModel
+    {
+        public ICollection<DisplayDegreeViewModel> Degrees { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Degrees/DisplayDegreeDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayDegreeDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayDegreeViewModel> degreeViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayDegreeDataTableViewModel certificateDataTableViewModel = new DisplayDegreeDataTableViewModel
+            {
+                Degrees = degreeViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayDegreeDataTable", certificateDataTableViewModel);
+        }
+    }
+}
