f8bf6b3333f51ac74666145c545e28e899011d7e
Łukasz Czepielik <koragrex@gmail.com>
2019-08-31 23:02:13 +0200

    Added DisplayGivenCertificateDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayGivenCertificateDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayGivenCertificateDataTableViewModel
+    {
+        public ICollection<DisplayGivenCertificateViewModel> GivenCertificates { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/GivenCertificates/DisplayGivenCertificateDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayGivenCertificateDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayGivenCertificateViewModel> givenCertificateViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayGivenCertificateDataTableViewModel givenCertificateDataTableViewModel = new DisplayGivenCertificateDataTableViewModel
+            {
+                GivenCertificates = givenCertificateViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayGivenCertificateDataTable", givenCertificateDataTableViewModel);
+        }
+    }
+}
