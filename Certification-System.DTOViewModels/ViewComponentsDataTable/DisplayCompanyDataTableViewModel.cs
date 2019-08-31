fa10c5284c596e3e93784207fdda4eb3192a739b
Łukasz Czepielik <koragrex@gmail.com>
2019-08-31 17:54:46 +0200

    Added DisplayCompanyDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayCompanyDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayCompanyDataTableViewModel
+    {
+        public ICollection<DisplayCompanyViewModel> Companies { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
