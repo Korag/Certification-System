a148bac63ac04d699d9b29bdb28bf7829b81c1e3
Łukasz Czepielik <koragrex@gmail.com>
2019-09-01 01:18:29 +0200

    Added DisplayCrucialDataWithContactUserDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayCrucialDataWithContactDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayCrucialDataWithContactDataTableViewModel
+    {
+        public ICollection<DisplayCrucialDataWithContactUserViewModel> Users { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Users/DisplayCrucialDataWithContactUserDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayCrucialDataWithContactUserDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<DisplayCrucialDataWithContactUserViewModel> userViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayCrucialDataWithContactDataTableViewModel userDataTableViewModel = new DisplayCrucialDataWithContactDataTableViewModel
+            {
+                Users = userViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayCrucialDataWithContactDataTable", userDataTableViewModel);
+        }
+    }
+}
