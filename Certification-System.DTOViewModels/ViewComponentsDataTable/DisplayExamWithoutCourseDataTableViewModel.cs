dae67955907b4ae47ce751593161fe2ddcb5237b
Łukasz Czepielik <koragrex@gmail.com>
2019-09-01 02:00:09 +0200

    Added DisplayExamWithoutCourseDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayExamWithoutCourseDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayExamWithoutCourseDataTableViewModel
+    {
+        public ICollection<DisplayExamWithoutCourseViewModel> Exams { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Exams/DisplayExamWithoutCourseDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayExamWithoutCourseDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayExamWithoutCourseViewModel> examViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayExamWithoutCourseDataTableViewModel examDataTableViewModel = new DisplayExamWithoutCourseDataTableViewModel
+            {
+                Exams = examViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayExamWithoutCourseDataTable", examDataTableViewModel);
+        }
+    }
+}
