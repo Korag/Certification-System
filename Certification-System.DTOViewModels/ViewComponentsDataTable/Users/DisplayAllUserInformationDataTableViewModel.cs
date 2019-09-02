d4b7c07f4bfa33eea063e8aed2fd050856cc9c68
Łukasz Czepielik <koragrex@gmail.com>
2019-09-03 01:08:10 +0200

    Added DisplayAllUserInformationDataTableViewComponent with appropriate ViewModels.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Users/DisplayAllUserInformationDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayAllUserInformationDataTableViewModel
+    {
+        public ICollection<DisplayAllUserInformationViewModel> Users { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Users/DisplayAllUserInformationDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayAllUserInformationDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<DisplayAllUserInformationViewModel> userViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayAllUserInformationDataTableViewModel userDataTableViewModel = new DisplayAllUserInformationDataTableViewModel
+            {
+                Users = userViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayAllUserInformationDataTable", userDataTableViewModel);
+        }
+    }
+}
