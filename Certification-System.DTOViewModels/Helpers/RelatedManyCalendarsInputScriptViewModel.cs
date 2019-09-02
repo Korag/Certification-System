0161188462bcd88e5b035bfc5608d1a651e8bf6a
Łukasz Czepielik <koragrex@gmail.com>
2019-09-02 18:39:23 +0200

    Added _RelatedManyCalendarsInputScript.

---
+++ Certification-System.DTOViewModels/Helpers/RelatedManyCalendarsInputScriptViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Certification_System.DTOViewModels
+{
+    public class RelatedManyCalendarsInputScriptViewModel
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public int QuantityOfInputs { get; set; }
+    }
+}
