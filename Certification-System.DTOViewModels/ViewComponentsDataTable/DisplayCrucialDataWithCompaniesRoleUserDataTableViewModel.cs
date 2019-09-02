805024337c23f8e38d8abb83eed3e1c562a5544a
Łukasz Czepielik <koragrex@gmail.com>
2019-09-02 19:51:17 +0200

    Fixed missing Course field in DisplayCrucialDataWithCompaniesRoleUserDataTableViewComponent.

--- Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayCrucialDataWithCompaniesRoleUserDataTableViewModel.cs
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/DisplayCrucialDataWithCompaniesRoleUserDataTableViewModel.cs
@@ -5,6 +5,7 @@ namespace Certification_System.DTOViewModels
     public class DisplayCrucialDataWithCompaniesRoleUserDataTableViewModel
     {
         public ICollection<DisplayCrucialDataWithCompaniesRoleUserViewModel> Users { get; set; }
+        public DisplayCourseViewModel Course { get; set; }
         public DataTableOptionsViewModel Options { get; set; }
     }
 }
--- Certification-System/Components/Users/DisplayCrucialDataWithCompaniesRoleUserDataTableViewComponent.cs
+++ Certification-System/Components/Users/DisplayCrucialDataWithCompaniesRoleUserDataTableViewComponent.cs
@@ -6,11 +6,12 @@ namespace Certification_System.Components
 {
     public class DisplayCrucialDataWithCompaniesRoleUserDataTableViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(ICollection<DisplayCrucialDataWithCompaniesRoleUserViewModel> userViewModel, string tableIdentificator, int operationSet = 0)
+        public IViewComponentResult Invoke(ICollection<DisplayCrucialDataWithCompaniesRoleUserViewModel> userViewModel, DisplayCourseViewModel courseViewModel, string tableIdentificator, int operationSet = 0)
         {
             DisplayCrucialDataWithCompaniesRoleUserDataTableViewModel userDataTableViewModel = new DisplayCrucialDataWithCompaniesRoleUserDataTableViewModel
             {
                 Users = userViewModel,
+                Course = courseViewModel,
                 Options = new DataTableOptionsViewModel
                 {
                     TableIdentificator = tableIdentificator,
