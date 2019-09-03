abbbe407ebf81c159d63588e237ddd07bfd090c2
Łukasz Czepielik <koragrex@gmail.com>
2019-09-03 18:36:44 +0200

    Added DisplayGivenCertificateToUserDataTableViewComponent with appropriate ViewModels.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/GivenCertificates/DisplayGivenCertificateToUserDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayGivenCertificateToUserDataTableViewModel
+    {
+        public ICollection<DisplayGivenCertificateToUserViewModel> GivenCertificates { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/GivenCertificates/DisplayGivenCertificateToUserDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayGivenCertificateToUserDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayGivenCertificateToUserViewModel> givenCertificateViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayGivenCertificateToUserDataTableViewModel givenCertificateDataTableViewModel = new DisplayGivenCertificateToUserDataTableViewModel
+            {
+                GivenCertificates = givenCertificateViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayGivenCertificateToUserDataTableV", givenCertificateDataTableViewModel);
+        }
+    }
+}
