f240f87466a57675dbd1469ba01f9e7c7d117bed
Łukasz Czepielik <koragrex@gmail.com>
2019-09-03 18:42:12 +0200

    Added DisplayGivenDegreeToUserDataTableViewComponent with appropriate ViewModels.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/GivenDegrees/DisplayGivenDegreeToUserDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayGivenDegreeToUserDataTableViewModel
+    {
+        public ICollection<DisplayGivenDegreeToUserViewModel> GivenDegrees { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/GivenDegrees/DisplayGivenDegreeToUserDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayGivenDegreeToUserDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayGivenDegreeToUserViewModel> givenDegreeViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayGivenDegreeToUserDataTableViewModel givenDegreeDataTableViewModel = new DisplayGivenDegreeToUserDataTableViewModel
+            {
+                GivenDegrees = givenDegreeViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayGivenDegreeToUserDataTable", givenDegreeDataTableViewModel);
+        }
+    }
+}
