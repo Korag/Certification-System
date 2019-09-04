76aa86d1c04af1315bc603920cee877b568f00d5
Łukasz Czepielik <koragrex@gmail.com>
2019-09-04 23:38:26 +0200

    Added DisplayMeetingWithoutInstructorDataTableViewComponent with appropriate ViewModels.

---
+++ Certification-System/Components/Meetings/DisplayMeetingWithoutInstructorDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayMeetingWithoutInstructorDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayMeetingWithoutInstructorViewModel> meetingViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayMeetingWithoutInstructorDataTableViewModel meetingDataTableViewModel = new DisplayMeetingWithoutInstructorDataTableViewModel
+            {
+                Meetings = meetingViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayMeetingWithoutInstructorDataTable", meetingDataTableViewModel);
+        }
+    }
+}
