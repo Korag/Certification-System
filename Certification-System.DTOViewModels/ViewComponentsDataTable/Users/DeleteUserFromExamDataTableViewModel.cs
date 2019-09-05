03b46894be8b5497b178259ae1daab5d9fa2e143
Łukasz Czepielik <koragrex@gmail.com>
2019-09-05 15:00:22 +0200

    Added DeleteUserFromExamDataTableViewComponent with appropriate ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Users/DeleteUserFromExamDataTableViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DeleteUserFromExamDataTableViewModel
+    {
+        public ICollection<DisplayCrucialDataUserViewModel> Users { get; set; }
+        public DeleteUsersFromCheckBoxViewModel[] UsersToDeleteFromExam { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Users/DeleteUserFromExamDataTableViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DeleteUserFromExamDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<DisplayCrucialDataUserViewModel> userViewModel, DeleteUsersFromCheckBoxViewModel[] userToDeleteViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DeleteUserFromExamDataTableViewModel userDataTableViewModel = new DeleteUserFromExamDataTableViewModel
+            {
+                Users = userViewModel,
+                UsersToDeleteFromExam = userToDeleteViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DeleteUserFromExamDataTable", userDataTableViewModel);
+        }
+    }
+}
