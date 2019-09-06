4feb2c3ee28fa6c3425d559600df0282e29a49a4
Łukasz Czepielik <koragrex@gmail.com>
2019-09-07 00:45:03 +0200

    Added MarkExamTermDataTableViewComponent with appropriate ViewModels.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/ExamsTerms/MarkExamTermDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class MarkExamTermDataTableViewModel
+    {
+        public ICollection<MarkUserViewModel> Users { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/ExamTerms/MarkExamTermDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class MarkExamTermDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<MarkUserViewModel> userViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            MarkExamTermDataTableViewModel examTermDataTableViewModel = new MarkExamTermDataTableViewModel
+            {
+                Users = userViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_MarkExamTermDataTable", examTermDataTableViewModel);
+        }
+    }
+}
