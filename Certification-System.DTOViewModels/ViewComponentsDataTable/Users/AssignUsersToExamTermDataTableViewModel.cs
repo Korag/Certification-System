fa27015681d498fc9670947e458d72dda751d35a
Łukasz Czepielik <koragrex@gmail.com>
2019-09-06 02:08:44 +0200

    Added AssignUsersToExamTermDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Users/AssignUsersToExamTermDataTableViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class AssignUsersToExamTermDataTableViewModel
+    {
+        public ICollection<DisplayCrucialDataUserViewModel> Users { get; set; }
+        public AddUsersFromCheckBoxViewModel[] UsersToAssignToExamTerm { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Users/AssignUsersToExamTermDataTableViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class AssignUsersToExamTermDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayCrucialDataUserViewModel> userViewModel, AddUsersFromCheckBoxViewModel[] userToAddViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            AssignUsersToExamTermDataTableViewModel userDataTableViewModel = new AssignUsersToExamTermDataTableViewModel
+            {
+                Users = userViewModel,
+                UsersToAssignToExamTerm = userToAddViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_AssignUsersToExamTermDataTable", userDataTableViewModel);
+        }
+    }
+}
