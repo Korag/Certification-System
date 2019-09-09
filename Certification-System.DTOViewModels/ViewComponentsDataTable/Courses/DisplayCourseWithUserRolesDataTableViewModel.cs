dcb717be5c1c6e9fe5f5cfb567874ea553c42daa
Łukasz Czepielik <koragrex@gmail.com>
2019-09-10 01:40:57 +0200

    Added DisplayCourseWithRolesDataTableViewComponent with appropriate View and ViewModels.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Courses/DisplayCourseWithUserRolesDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayCourseWithUserRolesDataTableViewModel
+    {
+        public ICollection<DisplayCourseWithUserRoleViewModel> Courses { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Courses/DisplayCourseWithRolesDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayCourseWithRolesDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayCourseWithUserRoleViewModel> courseViewModel, string tableIdentificator, int operationSet, string userIdentificator = null)
+        {
+            DisplayCourseWithUserRolesDataTableViewModel courseDataTableViewModel = new DisplayCourseWithUserRolesDataTableViewModel
+            {
+                Courses = courseViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayCourseWithRolesDataTable", courseDataTableViewModel);
+        }
+    }
+}
