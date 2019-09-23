48a8252a348bb40e9e990d720fee322eaf1cf187
Łukasz Czepielik <koragrex@gmail.com>
2019-09-24 01:27:40 +0200

    Added DisplayExamTermWithLocationDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/ExamsTerms/DisplayExamTermWithLocationDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayExamTermWithLocationDataTableViewModel
+    {
+        public ICollection<DisplayExamTermWithLocationViewModel> ExamsTerms{ get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/ExamTerms/DisplayExamTermWithLocationDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayExamTermWithLocationDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayExamTermWithLocationViewModel> examTermViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayExamTermWithLocationDataTableViewModel examTermDataTableViewModel = new DisplayExamTermWithLocationDataTableViewModel
+            {
+                ExamsTerms = examTermViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayExamTermWithLocationDataTable", examTermDataTableViewModel);
+        }
+    }
+}
