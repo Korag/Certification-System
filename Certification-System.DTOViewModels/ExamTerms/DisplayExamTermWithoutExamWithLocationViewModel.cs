b6ba78ca518ce5e754a45e35fb0204e6b8d4e7bb
Łukasz Czepielik <koragrex@gmail.com>
2019-09-24 01:58:41 +0200

    Added DisplayExamTermWtExamWhLocationDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ExamTerms/DisplayExamTermWithoutExamWithLocationViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayExamTermWithoutExamWithLocationViewModel
+    {
+        public string ExamTermIdentificator { get; set; }
+
+        [Display(Name = "Identyfikator")]
+        public string ExamTermIndexer { get; set; }
+
+        [Display(Name = "Data rozpoczęcia")]
+        public DateTime DateOfStart { get; set; }
+
+        [Display(Name = "Data zakończenia")]
+        public DateTime DateOfEnd { get; set; }
+
+        [Display(Name = "Czas [dni]")]
+        public int DurationDays { get; set; }
+
+        [Display(Name = "Czas [min]")]
+        public int DurationMinutes { get; set; }
+
+        [Display(Name = "Liczba uczestników")]
+        public int UsersQuantity { get; set; }
+
+        [Display(Name = "Limit uczestników")]
+        public int UsersLimit { get; set; }
+
+        [Display(Name = "Egzaminatorzy")]
+        public ICollection<DisplayCrucialDataUserViewModel> Examiners { get; set; }
+
+        [Display(Name = "Kraj")]
+        public string Country { get; set; }
+
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
+        public string PostCode { get; set; }
+
+        [Display(Name = "Ulica")]
+        public string Address { get; set; }
+
+        [Display(Name = "Numer domu/mieszkania")]
+        public string NumberOfApartment { get; set; }
+    }
+}
---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/ExamsTerms/DisplayExamTermWithoutExamWithLocationDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayExamTermWithoutExamWithLocationDataTableViewModel
+    {
+        public ICollection<DisplayExamTermWithoutExamWithLocationViewModel> ExamsTerms{ get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
