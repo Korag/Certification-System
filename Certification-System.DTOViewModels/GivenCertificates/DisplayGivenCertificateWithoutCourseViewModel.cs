063a9548e251b6971a9a03b093ad603e9215e8f1
Łukasz Czepielik <koragrex@gmail.com>
2019-10-03 00:25:58 +0200

    Added DisplayGivenCertificateWithoutCourseDataTableViewComponent.

---
+++ Certification-System.DTOViewModels/GivenCertificates/DisplayGivenCertificateWithoutCourseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayGivenCertificateWithoutCourseViewModel
+    {
+        public string GivenCertificateIdentificator { get; set; }
+
+        [Display(Name = "Identyfikator nadanego certyfikatu")]
+        public string GivenCertificateIndexer { get; set; }
+
+        [Display(Name = "Data otrzymania")]
+        public DateTime ReceiptDate { get; set; }
+
+        [Display(Name = "Data wygaśnięcia")]
+        public DateTime ExpirationDate { get; set; }
+
+        [Display(Name = "Nadany certyfikat")]
+        public DisplayCrucialDataCertificateViewModel Certificate { get; set; }
+
+        [Display(Name = "Użytkownik")]
+        public DisplayCrucialDataUserViewModel User { get; set; }
+    }
+}
---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/GivenCertificates/DisplayGivenCertificateWithoutCourseDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayGivenCertificateWithoutCourseDataTableViewModel
+    {
+        public ICollection<DisplayGivenCertificateWithoutCourseViewModel> GivenCertificates { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
