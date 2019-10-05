2522f38c4993263b1dce045ea74fab9b98c5d5f3
Łukasz Czepielik <koragrex@gmail.com>
2019-10-05 23:12:49 +0200

    Added WorkerCompetences action to UsersController with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/Users/WorkerCompetencesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class WorkerCompetencesViewModel
+    {
+        public string UserIdentificator { get; set; }
+
+        public ICollection<DisplayGivenCertificateToUserViewModel> GivenCertificates { get; set; }
+        public ICollection<DisplayGivenDegreeToUserViewModel> GivenDegrees { get; set; }
+    }
+}
