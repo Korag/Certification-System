e597398d6f57c28ff5177374da648f7941924168
Łukasz Czepielik <koragrex@gmail.com>
2019-10-06 19:28:33 +0200

    Added DisplayMeetingPresenceToUserDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Meetings/DisplayMeetingWithUserPresenceInformationDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayMeetingWithUserPresenceInformationDataTableViewModel
+    {
+        public ICollection<DisplayMeetingWithUserPresenceInformation> Meetings { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Meetings/DisplayMeetingPresenceToUserDataTableViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayMeetingPresenceToUserDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayMeetingWithUserPresenceInformation> meetingViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayMeetingWithUserPresenceInformationDataTableViewModel meetingDataTableViewModel = new DisplayMeetingWithUserPresenceInformationDataTableViewModel
+            {
+                Meetings = meetingViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayMeetingPresenceToUserDataTable", meetingDataTableViewModel);
+        }
+    }
+}
