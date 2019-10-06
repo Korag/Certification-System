1f925825034aaec46ad07d5c3e9c41054c91fb0e
Łukasz Czepielik <koragrex@gmail.com>
2019-10-07 01:20:42 +0200

    Added DisplayCrucialDataExamDataTableViewComponent  witha appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Exams/DisplayCrucialDataExamDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayCrucialDataExamDataTableViewModel
+    {
+        public ICollection<DisplayCrucialDataExamViewModel> Exams { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Exams/DisplayCrucialDataExamDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayCrucialDataExamDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayCrucialDataExamViewModel> examViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayCrucialDataExamDataTableViewModel examDataTableViewModel = new DisplayCrucialDataExamDataTableViewModel
+            {
+                Exams = examViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayCrucialDataExamDataTable", examDataTableViewModel);
+        }
+    }
+}
