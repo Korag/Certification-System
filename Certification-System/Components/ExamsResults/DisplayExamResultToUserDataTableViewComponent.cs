1584a185beded5c33096958477c8dbb8f8d720cc
Łukasz Czepielik <koragrex@gmail.com>
2019-10-07 00:59:14 +0200

    Added DisplayExamResultToUserDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System/Components/ExamsResults/DisplayExamResultToUserDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayExamResultToUserDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayExamResultToUserViewModel> examResultViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayExamResultToUserDataTableViewModel examResultDataTableViewModel = new DisplayExamResultToUserDataTableViewModel
+            {
+                ExamsResults = examResultViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayExamResultToUserDataTable", examResultDataTableViewModel);
+        }
+    }
+}
