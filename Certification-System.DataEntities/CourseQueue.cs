97173c474d634c2386d25b92ba9d51ace4600e8e
Łukasz Czepielik <koragrex@gmail.com>
2019-10-14 12:44:57 +0200

    Added CourseQueue Entitiy model and CourseQueueUser model.

---
+++ Certification-System.DataEntities/CourseQueue.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Certification_System.Entities
+{
+    public class CourseQueue
+    {
+        public CourseQueue()
+        {
+            AwaitingUsers = new List<CourseQueueUser>();
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CourseIdentificator { get; set; }
+        public ICollection<CourseQueueUser> AwaitingUsers { get; set; }
+    }
+}
---
+++ Certification-System.DataEntities/CourseQueueUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Certification_System.Entities
+{
+    public class CourseQueueUser
+    {
+        public string CourseQueueUserIdentificator { get; set; }
+
+        public string UserIdentificator { get; set; }
+        public DateTime DateOfApplication { get; set; }
+    }
+}
