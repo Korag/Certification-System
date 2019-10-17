fb54856c27e4baf4dfeebc75c87502e1212d94a4
Łukasz Czepielik <koragrex@gmail.com>
2019-10-17 02:01:07 +0200

    Added DisplayCourseQueueWithDecisionDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Courses/DisplayCourseQueueWithDecisionDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayCourseQueueWithDecisionDataTableViewModel
+    {
+        public ICollection<CourseQueueNotificationViewModel> CoursesNotifications { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Courses/DisplayCourseQueueWithDecisionDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayCourseQueueWithDecisionDataTable : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<CourseQueueNotificationViewModel> courseViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayCourseQueueWithDecisionDataTableViewModel courseDataTableViewModel = new DisplayCourseQueueWithDecisionDataTableViewModel
+            {
+                CoursesNotifications = courseViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayCourseQueueWithDecisionDataTable", courseDataTableViewModel);
+        }
+    }
+}
