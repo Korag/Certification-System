99f9667da5af2c84a7298086062f2f06adea20d4
Łukasz Czepielik <koragrex@gmail.com>
2019-10-20 18:00:02 +0200

    Added DisplayRejectedUsersFromCourseQueueLogDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Courses/DisplayRejectedUsersFromCourseQueueLogDataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class DisplayRejectedUsersFromCourseQueueLogDataTableViewModel
+    {
+        public ICollection<DisplayRejectedUserLog> Logs { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Courses/DisplayRejectedUsersFromCourseQueueLogDataTableViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class DisplayRejectedUsersFromCourseQueueLogDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayRejectedUserLog> logViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            DisplayRejectedUsersFromCourseQueueLogDataTableViewModel userDataTableViewModel = new DisplayRejectedUsersFromCourseQueueLogDataTableViewModel
+            {
+                Logs = logViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_DisplayRejectedUsersFromCourseQueueLogDataTable", userDataTableViewModel);
+        }
+    }
+}
