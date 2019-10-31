55a97a43e3fb1261db9bf82f8aece4abb08b0034
Łukasz Czepielik <koragrex@gmail.com>
2019-10-31 01:49:55 +0100

    Added AssignCompanyWorkersToCourseDataTableViewComponent with appropriate View and ViewModel.

---
+++ Certification-System.DTOViewModels/ViewComponentsDataTable/Courses/AssignCompanyWorkersToCourseDataTableViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Certification_System.DTOViewModels
+{
+    public class AssignCompanyWorkersToCourseDataTableViewModel
+    {
+        public ICollection<DisplayCrucialDataUserViewModel> Users { get; set; }
+        public AddUsersFromCheckBoxViewModel[] CompanyWorkersToAssignToCourse { get; set; }
+        public DataTableOptionsViewModel Options { get; set; }
+    }
+}
---
+++ Certification-System/Components/Courses/AssignCompanyWorkersToCourseDataTableViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Certification_System.DTOViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Certification_System.Components
+{
+    public class AssignCompanyWorkersToCourseDataTableViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<DisplayCrucialDataUserViewModel> userViewModel, AddUsersFromCheckBoxViewModel[] userToAddViewModel, string tableIdentificator, int operationSet = 0)
+        {
+            AssignCompanyWorkersToCourseDataTableViewModel userDataTableViewModel = new AssignCompanyWorkersToCourseDataTableViewModel
+            {
+                Users = userViewModel,
+                CompanyWorkersToAssignToCourse = userToAddViewModel,
+                Options = new DataTableOptionsViewModel
+                {
+                    TableIdentificator = tableIdentificator,
+                    OperationSet = operationSet
+                }
+            };
+
+            return View("_AssignCompanyWorkersToCourseDataTable", userDataTableViewModel);
+        }
+    }
+}
