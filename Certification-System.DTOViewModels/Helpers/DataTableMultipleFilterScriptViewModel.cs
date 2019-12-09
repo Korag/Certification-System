a9bbcd8b20c76c425d7477a1ab8051017883c55a
Łukasz Czepielik <koragrex@gmail.com>
2019-12-10 00:38:54 +0100

    Added _DataTableEllipsisScript to _DataTableWithMultipleFilterOnScript.

--- Certification-System.DTOViewModels/Helpers/DataTableMultipleFilterScriptViewModel.cs
+++ Certification-System.DTOViewModels/Helpers/DataTableMultipleFilterScriptViewModel.cs
@@ -7,5 +7,20 @@ namespace Certification_System.DTOViewModels
         public string TableIdentificator { get; set; }
         public ICollection<string> NumberOfColumn { get; set; }
         public ICollection<string> SelectId { get; set; }
+
+        public int[] EllipsisTargets { get; set; }
+        public int MaxAmountOfCharacters { get; set; }
+        public bool WordBreak { get; set; }
+
+        public DataTableMultipleFilterScriptViewModel(string tableIdentificator, ICollection<string> numberOfColumn, ICollection<string> selectId, int[] ellipsisTargets, int maxAmountOfCharacters, bool wordBreak)
+        {
+            this.TableIdentificator = tableIdentificator;
+            this.NumberOfColumn = numberOfColumn;
+            this.SelectId = selectId;
+
+            this.EllipsisTargets = ellipsisTargets;
+            this.MaxAmountOfCharacters = maxAmountOfCharacters;
+            this.WordBreak = wordBreak;
+        }
     }
 }
