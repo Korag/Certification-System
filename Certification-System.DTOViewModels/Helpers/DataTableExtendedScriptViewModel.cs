8ea9166bb645e5e83fbe53ffca5d3b4755658e35
Łukasz Czepielik <koragrex@gmail.com>
2019-12-10 00:32:05 +0100

    Used _DataTableEllipsisScript in _DataTableWithFilterOnColumnScript.

--- Certification-System.DTOViewModels/Helpers/DataTableExtendedScriptViewModel.cs
+++ Certification-System.DTOViewModels/Helpers/DataTableExtendedScriptViewModel.cs
@@ -5,5 +5,20 @@
         public string TableIdentificator { get; set; }
         public int NumberOfColumn { get; set; }
         public string SelectId { get; set; }
+
+        public int[] EllipsisTargets { get; set; }
+        public int MaxAmountOfCharacters { get; set; }
+        public bool WordBreak { get; set; }
+
+        public DataTableExtendedScriptViewModel(string tableIdentificator, int numberOfColumn, string selectId, int[] ellipsisTargets, int maxAmountOfCharacters, bool wordBreak)
+        {
+            this.TableIdentificator = tableIdentificator;
+            this.NumberOfColumn = numberOfColumn;
+            this.SelectId = selectId;
+
+            this.EllipsisTargets = ellipsisTargets;
+            this.MaxAmountOfCharacters = maxAmountOfCharacters;
+            this.WordBreak = wordBreak;
+        }
     }
 }
